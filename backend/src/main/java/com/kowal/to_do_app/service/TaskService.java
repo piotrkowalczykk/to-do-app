8a90ba254d0ab86564efe6c8965729c8e8b73824
meraj <merajmusavi1243m@gmail.com>
2024-05-29 12:30:12 +0330

    add mapper and dto pattern

--- backend/src/main/java/com/kowal/to_do_app/service/TaskService.java
+++ backend/src/main/java/com/kowal/to_do_app/service/TaskService.java
@@ -1,12 +1,13 @@
 package com.kowal.to_do_app.service;
 
+import com.kowal.to_do_app.dto.ShowTaskDto;
 import com.kowal.to_do_app.model.Task;
 
 import java.util.List;
 
 public interface TaskService {
     public Task saveTask(Task task);
-    public List<Task> getAllTasks();
+    public List<ShowTaskDto> getAllTasks();
     public void updateTaskStatus(Long id, Boolean status);
     public void deleteTask(Long id);
 
